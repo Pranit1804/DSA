7715581c2d818b649b356c20a0675f5a02da2913
pranit_bhogale <pranit@fitpage.in>
2024-10-17 22:02:37 +0530

    added factorial recurrsion

--- recurrsion/rec-factorial.dart
+++ recurrsion/rec-factorial.dart
@@ -1,3 +1,10 @@
 void main() {
-  print("Hello World");
+  print(isValidPalindrome(0, "MADAM"));
+}
+
+bool isValidPalindrome(int index, String n) {
+  int stringLength = n.length;
+  if (index >= stringLength) return true;
+  if (n[index] != n[stringLength - index - 1]) return false;
+  return isValidPalindrome(index + 1, n);
 }
